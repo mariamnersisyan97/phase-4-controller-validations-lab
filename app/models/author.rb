4e3e0dabe8b130e0430136c052b12db39bcd1d58
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2023-01-09 18:18:37 -0500

    commit tree d9453d40117baf2d19b3f1f57f10b9c2a0e04a2c

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    # name cannot be blank
+    validates :name, presence: true
+    #email is unique
+    validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    # title cannot be blank
+    validates :title, presence: true
+    #category is either "fiction" or "non-fiction"
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    #content is at least 100 characters long 
+    validates :content, length: { minimum: 100 }
 end
